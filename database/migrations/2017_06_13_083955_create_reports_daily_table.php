eee0e97c9fdcd19b614127201deedbbfe70caaaa
Dyas <dyas@live.com>
2017-06-13 15:41:32 +0700

    make migration reports_daily

---
+++ database/migrations/2017_06_13_083955_create_reports_daily_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReportsDailyTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('raports_daily', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('date');
+            $table->integer('amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('raports_daily', function (Blueprint $table) {
+            //
+        });
+    }
+}
