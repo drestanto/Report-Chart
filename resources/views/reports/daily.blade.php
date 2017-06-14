8061aa2cbe4f4cb28df6b4cf6e11bd8b3389f672
Dyas <dyas@live.com>
2017-06-14 13:26:11 +0700

    add hardcoded chart

--- resources/views/reports/daily.blade.php
+++ resources/views/reports/daily.blade.php
@@ -1,21 +1,24 @@
 <html>
+<head>
+	<title>Chart</title>
+	<script type="text/javascript" src="{!! asset('js/chart.min.js') !!}"></script>
+</head>
 <body>
 	<h1> Daily Resports </h1>
 	<canvas id="daily-reports" width="300" height="300"></canvas>
+	<script>
+		(function() {
+			var ctx = document.getElementById('daily-reports').getContext('2d');
+			var chart = {
+				labels: ['January', 'February', 'March'],
+				datasets: [{
+					data: [100, 423, 719]
+				}]
+			};
 
+			new Chart(ctx).Line(chart);
+		})();
+	</script>
 </body>
-<script src="/public/js/chart.min.js"></script>
-<script>
-	(function() {
-		var ctx = document.getElementById('daily-reports').getContext('2d');
-		var chart = {
-			labels: ['January', 'February', 'March'],
-			dataset: [{
-				data: [100, 423, 719]
-			}]
-		};
 
-		new Chart(ctx).Line(chart);
-	})();
-</script>
 </html>
