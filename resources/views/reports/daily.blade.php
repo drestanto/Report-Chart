7809ddc76fca8eefa802115bf69b6d7a474d6cde
Dyas <dyas@live.com>
2017-06-14 13:13:43 +0700

    init add chart

--- resources/views/reports/daily.blade.php
+++ resources/views/reports/daily.blade.php
@@ -1,6 +1,21 @@
 <html>
 <body>
 	<h1> Daily Resports </h1>
+	<canvas id="daily-reports" width="300" height="300"></canvas>
 
 </body>
+<script src="/public/js/chart.min.js"></script>
+<script>
+	(function() {
+		var ctx = document.getElementById('daily-reports').getContext('2d');
+		var chart = {
+			labels: ['January', 'February', 'March'],
+			dataset: [{
+				data: [100, 423, 719]
+			}]
+		};
+
+		new Chart(ctx).Line(chart);
+	})();
+</script>
 </html>
